da08b4ff27594e5bfa39997cf5b146d8507c41f8
Hampton Moore <me@hampton.pw>
2020-11-02 14:17:41 -0500

    Fix messages on login

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,61 +1,63 @@
-<div class="row mx-auto justify-content-center">
-    <h2>{message}</h2>
-    {#if state == states.ENDPOINT}
-        <div class="d-flex justify-content-center h-100">
-            <div class="d-flex justify-content-center form_container">
-                <form>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control input_user"
-                               placeholder="Oblecto server" bind:value={states.ENDPOINT.endpoint}>
-                    </div>
+<div class="mx-auto justify-content-center">
+    <h4 class="text-white text-center">{message}</h4>
+    <div class="row justify-content-center">
+        {#if state == states.ENDPOINT}
+            <div class="d-flex justify-content-center h-100">
+                <div class="d-flex justify-content-center form_container">
+                    <form>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control input_user"
+                                   placeholder="Oblecto server" bind:value={states.ENDPOINT.endpoint}>
+                        </div>
 
-                    <div class="d-flex justify-content-center mt-3 login_container">
-                        <button type="button" name="button" class="btn btn-primary" on:click={checkEndpoint}>Connect
-                        </button>
-                    </div>
-                </form>
+                        <div class="d-flex justify-content-center mt-3 login_container">
+                            <button type="button" name="button" class="btn btn-primary" on:click={checkEndpoint}>Connect
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    {/if}
+        {/if}
 
-    {#if state == states.USERS}
-            {#each states.USERS.users as user}
-                <div class="col-sm-12 col-md-4 col-lg-3 clickable bottom-pad" on:click={selectUser(user)}>
-                    <div class="card">
-                        <div class="img-wrapper">
-                            <img class="card-img-top" src="https://picsum.photos/seed/{user.username}/256/256">
-                            <div class="card-img-overlay d-flex">
-                                <h2 class="align-self-center mx-auto">{user.username}</h2>
+        {#if state == states.USERS}
+                {#each states.USERS.users as user}
+                    <div class="col-sm-12 col-md-4 col-lg-3 clickable bottom-pad" on:click={selectUser(user)}>
+                        <div class="card">
+                            <div class="img-wrapper">
+                                <img class="card-img-top" src="https://picsum.photos/seed/{user.username}/256/256">
+                                <div class="card-img-overlay d-flex">
+                                    <h2 class="align-self-center mx-auto">{user.username}</h2>
+                                </div>
                             </div>
                         </div>
                     </div>
+                {/each}
+        {/if}
+
+        {#if state == states.LOGIN}
+            <div class="d-flex justify-content-center h-100">
+                <div class="d-flex justify-content-center form_container">
+                    <form>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control input_user"
+                                   placeholder="Username" bind:value={states.LOGIN.username}>
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control input_user"
+                                   placeholder="Password" bind:value={states.LOGIN.password}>
+                        </div>
+
+                        <div class="d-flex justify-content-center mt-3 login_container">
+                            <button type="button" name="button" class="btn btn-primary" on:click={login}>
+                                Login
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            {/each}
-    {/if}
-
-    {#if state == states.LOGIN}
-        <div class="d-flex justify-content-center h-100">
-            <div class="d-flex justify-content-center form_container">
-                <form>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control input_user"
-                               placeholder="Username" bind:value={states.LOGIN.username}>
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control input_user"
-                               placeholder="Password" bind:value={states.LOGIN.password}>
-                    </div>
-
-                    <div class="d-flex justify-content-center mt-3 login_container">
-                        <button type="button" name="button" class="btn btn-primary" on:click={login}>
-                            Login
-                        </button>
-                    </div>
-                </form>
             </div>
-        </div>
-    {/if}
+        {/if}
+    </div>
 </div>
 
 <script>
@@ -87,7 +89,7 @@
         }
     }
     let state = states.ENDPOINT;
-    let message = '';
+    let message = 'Please type the URL of your Oblecto remote';
 
     async function checkEndpoint() {
         let result = await store.checkEndpoint(states.ENDPOINT.endpoint);
